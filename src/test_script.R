abdc3561cf95635b021eb9a3e061c919dcb6f38b
Brad Ross <bross35@stanford.edu>
2022-07-27 15:07:09 -0700

    added test_script.R

---
+++ src/test_script.R
@@ -0,0 +1,10 @@
+DATA_PATH = "./data"
+OUTPUT_PATH = "./output/temp"
+
+input_file = file(file.path(DATA_PATH, "input.txt"))
+input_contents = readLines(input_file)
+close(input_file)
+
+output_file = file(file.path(OUTPUT_PATH, "output.txt"))
+writeLines(toupper(input_contents), output_file)
+close(output_file)
